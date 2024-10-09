a00483e39a264eefb33a540891c2bd42010488af
ilya <sol1kek@estelink.ru>
2024-10-09 23:30:41 +0300

    create order handler

--- internal/handlers/postOrder.go
+++ internal/handlers/postOrder.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sol1corejz/gofermart/internal/auth"
 	"github.com/sol1corejz/gofermart/internal/logger"
 	"github.com/sol1corejz/gofermart/internal/storage"
+	"go.uber.org/zap"
 	"regexp"
 	"time"
 )
@@ -51,39 +52,52 @@ func CreateOrderHandler(c *fiber.Ctx) error {
 
 		if !luhnCheck.Match(orderNumber) {
 			logger.Log.Error("Invalid order number")
-			return c.Status(fiber.StatusUnprocessableEntity).SendString("Invalid order number")
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"error": "Invalid order number",
+			})
 		}
 
-		if !isValidLuhn(string(orderNumber)) {
-			logger.Log.Error("Invalid order number")
-			return c.Status(fiber.StatusUnprocessableEntity).SendString("Invalid order number")
-		}
+		//if !isValidLuhn(string(orderNumber)) {
+		//	logger.Log.Error("Invalid order number")
+		//	return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+		//		"error": "Invalid order number",
+		//	})
+		//}
 
 		order, err := storage.GetOrderByNumber(ctx, string(orderNumber))
 
+		if err != nil {
+			if !errors.Is(err, storage.ErrNoSuchOrder) {
+				logger.Log.Error("Error checking order", zap.Error(err))
+				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+					"error": "Error checking order",
+				})
+			}
+		}
+
 		if order.OrderNumber == string(orderNumber) && order.UserID == userID {
 			logger.Log.Info("Order number already registered by this user")
-			return c.Status(fiber.StatusOK).SendString("Order already registered by this user")
+			return c.Status(fiber.StatusOK).JSON(fiber.Map{
+				"message": "Order already registered by this user",
+			})
 		}
 
 		if order.OrderNumber != "" {
 			logger.Log.Info("Order number already exists")
-			return c.Status(fiber.StatusConflict).SendString("Order number already exists")
+			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+				"error": "Order number already exists",
+			})
 		}
 
+		err = storage.CreateOrder(ctx, userID.String(), string(orderNumber))
 		if err != nil {
-			if errors.Is(err, storage.ErrNoSuchOrder) {
-				err = storage.CreateOrder(ctx, userID.String(), string(orderNumber))
-				if err != nil {
-					return c.Status(fiber.StatusInternalServerError).SendString("Error creating order")
-				}
-				return c.Status(fiber.StatusAccepted).SendString("Order created")
-			}
-
-			logger.Log.Error("Error creating order")
-			return c.Status(fiber.StatusInternalServerError).SendString("Error creating order")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Error creating order",
+			})
 		}
+		return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+			"message": "Order created",
+		})
 
-		return nil
 	}
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -44,7 +44,7 @@ func Init() error {
 			user_id UUID NOT NULL REFERENCES users(id),
 			order_number VARCHAR(255) UNIQUE NOT NULL,
 			status VARCHAR(20) NOT NULL,
-			accrual DECIMAL(10, 2),
+			accrual DECIMAL(10, 2) DEFAULT 0.00,
 			uploaded_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 		);`,
 		`CREATE TABLE IF NOT EXISTS user_balances (
@@ -103,7 +103,7 @@ func CreateUser(ctx context.Context, userID string, login string, passwordHash s
 func CreateOrder(ctx context.Context, userID string, orderNumber string) error {
 
 	_, err := DB.ExecContext(ctx, `
-        INSERT INTO orders (user_id, order_number, status) VALUES ($1, $2, $3);
+        INSERT INTO orders (user_id, order_number, status) VALUES ($1, $2, $3) ON CONFLICT (order_number) DO NOTHING;
     `, userID, orderNumber, models.PROCESSING)
 
 	if err != nil {
