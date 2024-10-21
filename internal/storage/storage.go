8b8af3927e4593939ac08aaea9446ced96813dc9
ilya <sol1kek@estelink.ru>
2024-10-21 21:14:19 +0300

    Revert "loyality system worker"
    This reverts commit f0c2fe7803e6c57b8e2c670fd7b905f5b1fbb4b1.

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -269,11 +269,11 @@ func CreateWithdrawal(ctx context.Context, userID uuid.UUID, order string, sum f
 	return nil
 }
 
-func GetAllUnprocessedOrders(ctx context.Context) ([]models.Order, error) {
+func GetAllOrders(ctx context.Context) ([]models.Order, error) {
 	var orders []models.Order
 
 	rows, err := DB.QueryContext(ctx, `
-		SELECT * FROM orders WHERE status NOT IN ('INVALID', 'PROCESSED');;
+		SELECT * FROM orders;
 	`)
 
 	if err != nil {
