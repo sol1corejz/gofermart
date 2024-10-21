c6de7f7cf7f626dc30f4db70ff9351e31e12ec45
ilya <sol1kek@estelink.ru>
2024-10-21 21:14:15 +0300

    Revert "loyality system worker"
    This reverts commit 02ba183feaf9c1257c162906857259db207641bc.

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"fmt"
 	"github.com/google/uuid"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/sol1corejz/gofermart/cmd/config"
@@ -172,8 +171,6 @@ func GetUserOrders(ctx context.Context, UUID uuid.UUID) ([]models.Order, error) 
 }
 
 func GetOrderByNumber(ctx context.Context, orderNumber string) (models.Order, error) {
-
-	fmt.Println(784387487247823, orderNumber)
 
 	var order models.Order
 
